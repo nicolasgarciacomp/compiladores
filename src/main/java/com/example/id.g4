23778b54fc379dc81f9110f69f3afc54167fb4c7
Nicolas Garcia <nicolas.garcia@ross.com.ar>
2021-09-18 00:53:19 +0100

    Tarea 3

--- src/main/java/com/example/id.g4
+++ src/main/java/com/example/id.g4
@@ -29,28 +29,29 @@ OPERACION_N : '!' ;
 OP_A : (ID | NUMERO) (ESP OPERACION_A ESP (ID | NUMERO))+ ;
 OP_R : (ID | NUMERO) ESP OPERADORES_R ESP (ID | NUMERO) ;
 
+ASIG : ID ESP '=' ESP (ID | NUMERO) PYC ;
 INST_A : ID ESP '=' ESP (OP_A)+ PYC ;
 INST_R : PA OP_R PC ESP OPERACION_L ESP PA OP_R PC ;
 INST_R_ : OP_R ESP OPERACION_L ESP OP_R ;
 INST_N : OPERACION_N ESP PA OP_R PC ;
 
-WHILE : 'while' ESP PA (OP_R | INST_R_)+ PC WS LLA (WS+ INST_A)+ WS+ LLC ;
+WHILE : 'while' ESP PA (OP_R | INST_R_)+ PC (ESP LLA | WS LLA) (WS+ (INST_A | ASIG))+ WS+ LLC ;
+FOR : 'for' ESP PA ASIG ESP OP_R PYC ESP ID ESP '=' ESP (OP_A)+ PC ;
+IF : 'if' ESP PA (OP_R | INST_R_)+ PC (WS+ (INST_A | ASIG))+ WS+
+   | 'if' ESP PA (OP_R | INST_R_)+ PC (WS+ (INST_A | ASIG))+ WS+ 'else' ESP LLA (WS+ (INST_A | ASIG))+ WS+ LLC;
 
-INSTRUCCION : TIPO ESP ID PYC
-            | TIPO ESP ID ESP '=' ESP ID PYC
-            | TIPO ESP (ID', ')+ ID PYC
-            | TIPO ESP ((ID ESP '= ' (ID | NUMERO) ', ') | (ID ', '))+ ((ID ESP '= ' (ID | NUMERO) PYC) | (ID PYC))
-            ;
+DECLA : TIPO ESP ID PYC
+      | TIPO ESP ID ESP '=' ESP ID PYC
+      | TIPO ESP (ID', ')+ ID PYC
+      | TIPO ESP ((ID ESP '= ' (ID | NUMERO) ', ') | (ID ', '))+ ((ID ESP '= ' (ID | NUMERO) PYC) | (ID PYC))
+      ;
 
-s : WHILE { System.out.println("WHILE ->" + $WHILE.getText() + "<--"); } s
-  | OP_A { System.out.println("OP_A ->" + $OP_A.getText() + "<--"); } s
-  | OP_R { System.out.println("OP_R ->" + $OP_R.getText() + "<--"); } s
+s : DECLA { System.out.println("DECLA ->" + $DECLA.getText() + "<--"); } s
+  | ASIG { System.out.println("ASIG ->" + $ASIG.getText() + "<--"); } s
   | INST_A { System.out.println("INST_A ->" + $INST_A.getText() + "<--"); } s
-  | INST_R { System.out.println("INST_R ->" + $INST_R.getText() + "<--"); } s
-  | INST_N { System.out.println("INST_N ->" + $INST_N.getText() + "<--"); } s
-  | INSTRUCCION { System.out.println("INSTRUCCION ->" + $INSTRUCCION.getText() + "<--"); } s
-  | ID { System.out.println("ID ->" + $ID.getText() + "<--"); } s
-  | NUMERO { System.out.println("NUMERO ->" + $NUMERO.getText() + "<--"); } s
+  | WHILE { System.out.println("WHILE ->" + $WHILE.getText() + "<--"); } s
+  | FOR { System.out.println("FOR ->" + $FOR.getText() + "<--"); } s
+  | IF { System.out.println("IF ->" + $IF.getText() + "<--"); } s
   | OTRO { System.out.println("Otro ->" + $OTRO.getText() + "<--"); } s
   | EOF
   ;
--- src/main/java/com/example/idParser.java
+++ src/main/java/com/example/idParser.java
@@ -21,8 +21,8 @@ public class idParser extends Parser {
 	public static final int
 		ID=1, NUMERO=2, OTRO=3, PA=4, PC=5, LLA=6, LLC=7, PYC=8, COMA=9, ESP=10, 
 		WS=11, TIPO=12, OPERACION_A=13, OPERADORES_R=14, OPERACION_L=15, OPERACION_N=16, 
-		OP_A=17, OP_R=18, INST_A=19, INST_R=20, INST_R_=21, INST_N=22, WHILE=23, 
-		INSTRUCCION=24;
+		OP_A=17, OP_R=18, ASIG=19, INST_A=20, INST_R=21, INST_R_=22, INST_N=23, 
+		WHILE=24, FOR=25, IF=26, DECLA=27;
 	public static final int
 		RULE_s = 0;
 	private static String[] makeRuleNames() {
@@ -43,7 +43,8 @@ public class idParser extends Parser {
 		return new String[] {
 			null, "ID", "NUMERO", "OTRO", "PA", "PC", "LLA", "LLC", "PYC", "COMA", 
 			"ESP", "WS", "TIPO", "OPERACION_A", "OPERADORES_R", "OPERACION_L", "OPERACION_N", 
-			"OP_A", "OP_R", "INST_A", "INST_R", "INST_R_", "INST_N", "WHILE", "INSTRUCCION"
+			"OP_A", "OP_R", "ASIG", "INST_A", "INST_R", "INST_R_", "INST_N", "WHILE", 
+			"FOR", "IF", "DECLA"
 		};
 	}
 	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
@@ -98,28 +99,22 @@ public class idParser extends Parser {
 	}
 
 	public static class SContext extends ParserRuleContext {
-		public Token WHILE;
-		public Token OP_A;
-		public Token OP_R;
+		public Token DECLA;
+		public Token ASIG;
 		public Token INST_A;
-		public Token INST_R;
-		public Token INST_N;
-		public Token INSTRUCCION;
-		public Token ID;
-		public Token NUMERO;
+		public Token WHILE;
+		public Token FOR;
+		public Token IF;
 		public Token OTRO;
-		public TerminalNode WHILE() { return getToken(idParser.WHILE, 0); }
+		public TerminalNode DECLA() { return getToken(idParser.DECLA, 0); }
 		public SContext s() {
 			return getRuleContext(SContext.class,0);
 		}
-		public TerminalNode OP_A() { return getToken(idParser.OP_A, 0); }
-		public TerminalNode OP_R() { return getToken(idParser.OP_R, 0); }
+		public TerminalNode ASIG() { return getToken(idParser.ASIG, 0); }
 		public TerminalNode INST_A() { return getToken(idParser.INST_A, 0); }
-		public TerminalNode INST_R() { return getToken(idParser.INST_R, 0); }
-		public TerminalNode INST_N() { return getToken(idParser.INST_N, 0); }
-		public TerminalNode INSTRUCCION() { return getToken(idParser.INSTRUCCION, 0); }
-		public TerminalNode ID() { return getToken(idParser.ID, 0); }
-		public TerminalNode NUMERO() { return getToken(idParser.NUMERO, 0); }
+		public TerminalNode WHILE() { return getToken(idParser.WHILE, 0); }
+		public TerminalNode FOR() { return getToken(idParser.FOR, 0); }
+		public TerminalNode IF() { return getToken(idParser.IF, 0); }
 		public TerminalNode OTRO() { return getToken(idParser.OTRO, 0); }
 		public TerminalNode EOF() { return getToken(idParser.EOF, 0); }
 		public SContext(ParserRuleContext parent, int invokingState) {
@@ -145,113 +140,83 @@ public class idParser extends Parser {
 		SContext _localctx = new SContext(_ctx, getState());
 		enterRule(_localctx, 0, RULE_s);
 		try {
-			setState(33);
+			setState(24);
 			_errHandler.sync(this);
 			switch (_input.LA(1)) {
-			case WHILE:
+			case DECLA:
 				enterOuterAlt(_localctx, 1);
 				{
 				setState(2);
-				((SContext)_localctx).WHILE = match(WHILE);
-				 System.out.println("WHILE ->" + ((SContext)_localctx).WHILE.getText() + "<--"); 
+				((SContext)_localctx).DECLA = match(DECLA);
+				 System.out.println("DECLA ->" + ((SContext)_localctx).DECLA.getText() + "<--"); 
 				setState(4);
 				s();
 				}
 				break;
-			case OP_A:
+			case ASIG:
 				enterOuterAlt(_localctx, 2);
 				{
 				setState(5);
-				((SContext)_localctx).OP_A = match(OP_A);
-				 System.out.println("OP_A ->" + ((SContext)_localctx).OP_A.getText() + "<--"); 
+				((SContext)_localctx).ASIG = match(ASIG);
+				 System.out.println("ASIG ->" + ((SContext)_localctx).ASIG.getText() + "<--"); 
 				setState(7);
 				s();
 				}
 				break;
-			case OP_R:
+			case INST_A:
 				enterOuterAlt(_localctx, 3);
 				{
 				setState(8);
-				((SContext)_localctx).OP_R = match(OP_R);
-				 System.out.println("OP_R ->" + ((SContext)_localctx).OP_R.getText() + "<--"); 
+				((SContext)_localctx).INST_A = match(INST_A);
+				 System.out.println("INST_A ->" + ((SContext)_localctx).INST_A.getText() + "<--"); 
 				setState(10);
 				s();
 				}
 				break;
-			case INST_A:
+			case WHILE:
 				enterOuterAlt(_localctx, 4);
 				{
 				setState(11);
-				((SContext)_localctx).INST_A = match(INST_A);
-				 System.out.println("INST_A ->" + ((SContext)_localctx).INST_A.getText() + "<--"); 
+				((SContext)_localctx).WHILE = match(WHILE);
+				 System.out.println("WHILE ->" + ((SContext)_localctx).WHILE.getText() + "<--"); 
 				setState(13);
 				s();
 				}
 				break;
-			case INST_R:
+			case FOR:
 				enterOuterAlt(_localctx, 5);
 				{
 				setState(14);
-				((SContext)_localctx).INST_R = match(INST_R);
-				 System.out.println("INST_R ->" + ((SContext)_localctx).INST_R.getText() + "<--"); 
+				((SContext)_localctx).FOR = match(FOR);
+				 System.out.println("FOR ->" + ((SContext)_localctx).FOR.getText() + "<--"); 
 				setState(16);
 				s();
 				}
 				break;
-			case INST_N:
+			case IF:
 				enterOuterAlt(_localctx, 6);
 				{
 				setState(17);
-				((SContext)_localctx).INST_N = match(INST_N);
-				 System.out.println("INST_N ->" + ((SContext)_localctx).INST_N.getText() + "<--"); 
+				((SContext)_localctx).IF = match(IF);
+				 System.out.println("IF ->" + ((SContext)_localctx).IF.getText() + "<--"); 
 				setState(19);
 				s();
 				}
 				break;
-			case INSTRUCCION:
+			case OTRO:
 				enterOuterAlt(_localctx, 7);
 				{
 				setState(20);
-				((SContext)_localctx).INSTRUCCION = match(INSTRUCCION);
-				 System.out.println("INSTRUCCION ->" + ((SContext)_localctx).INSTRUCCION.getText() + "<--"); 
+				((SContext)_localctx).OTRO = match(OTRO);
+				 System.out.println("Otro ->" + ((SContext)_localctx).OTRO.getText() + "<--"); 
 				setState(22);
 				s();
 				}
 				break;
-			case ID:
+			case EOF:
 				enterOuterAlt(_localctx, 8);
 				{
 				setState(23);
-				((SContext)_localctx).ID = match(ID);
-				 System.out.println("ID ->" + ((SContext)_localctx).ID.getText() + "<--"); 
-				setState(25);
-				s();
-				}
-				break;
-			case NUMERO:
-				enterOuterAlt(_localctx, 9);
-				{
-				setState(26);
-				((SContext)_localctx).NUMERO = match(NUMERO);
-				 System.out.println("NUMERO ->" + ((SContext)_localctx).NUMERO.getText() + "<--"); 
-				setState(28);
-				s();
-				}
-				break;
-			case OTRO:
-				enterOuterAlt(_localctx, 10);
-				{
-				setState(29);
-				((SContext)_localctx).OTRO = match(OTRO);
-				 System.out.println("Otro ->" + ((SContext)_localctx).OTRO.getText() + "<--"); 
-				setState(31);
-				s();
-				}
-				break;
-			case EOF:
-				enterOuterAlt(_localctx, 11);
-				{
-				setState(32);
 				match(EOF);
 				}
 				break;
@@ -271,17 +236,15 @@ public class idParser extends Parser {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\32&\4\2\t\2\3\2\3"+
-		"\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2"+
-		"\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\5\2$\n\2\3\2\2\2\3\2"+
-		"\2\2\2.\2#\3\2\2\2\4\5\7\31\2\2\5\6\b\2\1\2\6$\5\2\2\2\7\b\7\23\2\2\b"+
-		"\t\b\2\1\2\t$\5\2\2\2\n\13\7\24\2\2\13\f\b\2\1\2\f$\5\2\2\2\r\16\7\25"+
-		"\2\2\16\17\b\2\1\2\17$\5\2\2\2\20\21\7\26\2\2\21\22\b\2\1\2\22$\5\2\2"+
-		"\2\23\24\7\30\2\2\24\25\b\2\1\2\25$\5\2\2\2\26\27\7\32\2\2\27\30\b\2\1"+
-		"\2\30$\5\2\2\2\31\32\7\3\2\2\32\33\b\2\1\2\33$\5\2\2\2\34\35\7\4\2\2\35"+
-		"\36\b\2\1\2\36$\5\2\2\2\37 \7\5\2\2 !\b\2\1\2!$\5\2\2\2\"$\7\2\2\3#\4"+
-		"\3\2\2\2#\7\3\2\2\2#\n\3\2\2\2#\r\3\2\2\2#\20\3\2\2\2#\23\3\2\2\2#\26"+
-		"\3\2\2\2#\31\3\2\2\2#\34\3\2\2\2#\37\3\2\2\2#\"\3\2\2\2$\3\3\2\2\2\3#";
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\3\35\35\4\2\t\2\3\2"+
+		"\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3\2\3"+
+		"\2\3\2\3\2\3\2\5\2\33\n\2\3\2\2\2\3\2\2\2\2\"\2\32\3\2\2\2\4\5\7\35\2"+
+		"\2\5\6\b\2\1\2\6\33\5\2\2\2\7\b\7\25\2\2\b\t\b\2\1\2\t\33\5\2\2\2\n\13"+
+		"\7\26\2\2\13\f\b\2\1\2\f\33\5\2\2\2\r\16\7\32\2\2\16\17\b\2\1\2\17\33"+
+		"\5\2\2\2\20\21\7\33\2\2\21\22\b\2\1\2\22\33\5\2\2\2\23\24\7\34\2\2\24"+
+		"\25\b\2\1\2\25\33\5\2\2\2\26\27\7\5\2\2\27\30\b\2\1\2\30\33\5\2\2\2\31"+
+		"\33\7\2\2\3\32\4\3\2\2\2\32\7\3\2\2\2\32\n\3\2\2\2\32\r\3\2\2\2\32\20"+
+		"\3\2\2\2\32\23\3\2\2\2\32\26\3\2\2\2\32\31\3\2\2\2\33\3\3\2\2\2\3\32";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
